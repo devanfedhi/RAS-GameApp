67c07f0e2be59f018c77dd8ba38988559a66a3d8
devanfedhi <devanfedhi1@gmail.com>
2025-02-10 15:54:24 +1100

    test change port

--- gameapp-client/src/App.tsx
+++ gameapp-client/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-const socket = io(`http://localhost:4001`);
+const socket = io(`http://0.0.0.0:4001`);
 
 type Message = {
   user: string;
