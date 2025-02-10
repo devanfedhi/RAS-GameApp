64a434922729fa5b22649f5c2ef1faabdce1b888
devanfedhi <devanfedhi1@gmail.com>
2025-02-10 21:13:17 +1100

    test

--- gameapp-client/src/App.tsx
+++ gameapp-client/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-const socket = io('http://localhost:4001');
+const socket = io('https://www.otterhello.live');
 
 type Message = {
   user: string;
