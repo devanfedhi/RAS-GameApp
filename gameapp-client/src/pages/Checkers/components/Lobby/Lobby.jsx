87ec2f739cedab5c6ab27be5fdfea1dbbffa01ae
devanfedhi <devanfedhi1@gmail.com>
2025-02-16 17:13:08 +1100

    Implemented lobby feature + css

--- gameapp-client/src/pages/Checkers/components/Lobby/Lobby.jsx
+++ gameapp-client/src/pages/Checkers/components/Lobby/Lobby.jsx
@@ -5,41 +5,48 @@ import { PAGE_CREATE_NEW_GAME } from '../Props';
 function Lobby({ setPage, allGames }) {
 
   return (
-    <div>
+    <section className={styles.container}>
       <h2>Lobby</h2>
-      {/* Example usage of setPage */}
-      <button onClick={() => setPage(PAGE_CREATE_NEW_GAME)}>Create New Game</button>
+      <button className={styles.createButton} onClick={() => setPage(PAGE_CREATE_NEW_GAME)}>Create New Game</button>
 
 
-      <table>
+      <table className={styles.lobbyTable}> 
+        <colgroup>
+          <col style={{ width: '25%' }} />
+          <col style={{ width: '10%' }} />
+          <col style={{ width: '55%' }} />
+          <col style={{ width: '10%' }} />
+        </colgroup>
         <thead>
           <tr>
-            <th>Game Name</th>
-            <th>Number of Players</th>
-            <th>Players</th>
+            <th><h3>Game Name</h3></th>
+            <th><h3>Size</h3></th>
+            <th><h3>Players</h3></th>
+            <th><h3></h3></th>
           </tr>
         </thead>
-        <tbody>
-          {!allGames && (
-            <tr>
-              <td colSpan="3">Loading... no games created yet</td>
-            </tr>
-          )}
-          {allGames && allGames.map((game) => (
+        <tbody classname={styles.lobbyTableBody}>
+        {!allGames ? (
+          <tr>
+            <td colSpan="3">Loading... no games created yet</td>
+          </tr>
+        ) : (
+          allGames.map((game) => (
             <tr key={game.gameName}>
-              <td>{game.gameName}</td>
-              <td>{game.currentNumPlayers} / 2</td>
-              <td>
+              <td><p>{game.gameName}</p></td>
+              <td><p>{game.currentNumPlayers} / 2</p></td>
+              <td><p>
                 {game.playerNames.map((player) => (
                   <div key={player}>{player}</div>
                 ))}
-              </td>
-              <td><button>Join Game</button></td>
+              </p></td>
+              <td><button className={styles.joinButton}><p>Join</p></button></td>
             </tr>
-          ))}
+          ))
+        )}
         </tbody>
       </table>
-    </div>
+    </section>
   );
 };
 
